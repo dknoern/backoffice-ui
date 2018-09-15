666f10292da1249bb1cde9eed2b6a816d6dfc6cc
David Knoernschild <david@seattleweb.com>
2018-09-15 13:04:14 -0700

    redirect other uris

--- server.js
+++ server.js
@@ -5,4 +5,17 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+
+
+
+
+// Set our api routes
+//app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+
 app.listen(process.env.PORT || 8080);
